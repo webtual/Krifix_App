bcd5bb45fdb2bc751770a3d9524f7718820247ee
Webtual Technologies Pvt. Ltd <webtualtechnology@gmail.com>
2023-04-14 12:09:24 +0530

    production url

--- src/constants/ApiUrl.js
+++ src/constants/ApiUrl.js
@@ -1,5 +1,15 @@
-export const BASE_URL = 'https://dev.thewebtual.com/krifixrewards/api/'
-export const IMAGE_URL = "https://dev.thewebtual.com/krifixrewards/"
+
+// ======= dev enviroment=========
+// export const BASE_URL = 'https://dev.thewebtual.com/krifixrewards/api/'   
+// export const IMAGE_URL = "https://dev.thewebtual.com/krifixrewards/"  
+// ======= dev =========
+
+
+// ======= production enviroment  ========
+export const BASE_URL = 'https://rewardpanel.krifix.com/api/'  
+export const IMAGE_URL = "https://rewardpanel.krifix.com/"  
+// ======= production enviroment  ========
+
 export const LOGIN = "auth/login"
 export const CHECK_MOBILE = "auth/check-mobile"
 export const REGISTER = "auth/register"
